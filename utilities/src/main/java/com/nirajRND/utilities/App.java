c8ea4ff04e199a11b803796c9a9ae5b476955ca9
Niraj Jha <niraj.jha@synapse.com>
2019-04-09 16:19:59 +0530

    maven build setup of module

---
+++ utilities/src/main/java/com/nirajRND/utilities/App.java
@@ -0,0 +1,13 @@
+package com.nirajRND.utilities;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+   public int sum(int a, int b) {
+	   
+	   return a+b;
+   }
+}
---
+++ web/src/main/java/com/test/Test.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import com.nirajRND.utilities.App;
+
+public class Test {
+
+	public static void main(String[] args) {
+		App a1 = new App();
+		a1.sum(12, 12);
+
+	}
+
+}
